6b2dcd11a4471d5fd7a3b4c9647019edbcc3938d
Matthew Kimball <mekimba6@gmail.com>
2022-02-24 11:19:30 -0700

    adds more model tests

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Subscription, type: :model do
     it { should have_many :teas}
     it {should belong_to :customer}
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title)}
+    it { should validate_presence_of(:price)}
+    it { should validate_presence_of(:status)}
+    it { should validate_presence_of(:frequency)}
+  end
 end
