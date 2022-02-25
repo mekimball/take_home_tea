9544d3890be2cc7cf44ae8d1e7ba6b898a3b60e5
Matthew Kimball <mekimba6@gmail.com>
2022-02-24 19:09:13 -0700

    adds more seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
     tea_1 = Tea.create!(title: 'green tea', description: 'yummy', temperature: 179, brew_time: 5)
     tea_2 = Tea.create!(title: 'oolong tea', description: 'also yummy', temperature: 185, brew_time: 7)
     subscription_1 = Subscription.create!(title: 'basic', price: 5.99, status: 'active', frequency: 'every two weeks', customer_id: customer_1.id, tea_id: tea_1.id)
+    subscription_2 = Subscription.create!(title: 'bougie', price: 15.99, status: 'inactive', frequency: 'daily', customer_id: customer_1.id, tea_id: tea_1.id)
