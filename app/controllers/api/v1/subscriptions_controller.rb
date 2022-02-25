f42baab82fc8b9a64ac1da5566eb2e89c67b4c89
Matthew Kimball <mekimba6@gmail.com>
2022-02-24 17:52:05 -0700

    adds test and code for cancelling subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -3,6 +3,19 @@ class Api::V1::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(Subscription.create(subscription_params)), status: 201
   end
 
+  def update
+    updated_sub = Subscription.find_by(id: params[:id])
+    updated_sub.update(subscription_params)
+    render json: SubscriptionSerializer.new(updated_sub), status: 201
+  end
+
+  def index
+    customer = 
+    require 'pry'; binding.pry
+  end
+
+  private
+
   def subscription_params
     params.require(:subscription).permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
   end
